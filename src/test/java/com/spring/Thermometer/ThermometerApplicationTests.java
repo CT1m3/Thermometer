4ff6af52dc26895ef810f659fcf3ccf0439c3a20
CT1m3 <simas.banevicius26@gmail.com>
2024-05-19 19:27:57 +0300

    Initial commit

---
+++ src/test/java/com/spring/Thermometer/ThermometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Thermometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
