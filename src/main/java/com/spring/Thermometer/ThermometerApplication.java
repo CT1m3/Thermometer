d8b47aae52d91e908379762d9ee09de83df7f295
CT1m3 <simas.banevicius26@gmail.com>
2024-05-22 10:38:17 +0300

    Added 3D files

--- src/main/java/com/spring/Thermometer/ThermometerApplication.java
+++ src/main/java/com/spring/Thermometer/ThermometerApplication.java
@@ -14,6 +14,5 @@ public class ThermometerApplication {
 //TODO: Add successful register message
 //TODO: Add Websockets or something similar for better dashboard temperature refreshing
 //TODO: Configure status to show if device is connected and when you click it, it attempts to reconnect
-//TODO: Add 3d models to project
 //TODO: Admin sees all temps, users only see their own
 //TODO: Add searchbar to dashboard
